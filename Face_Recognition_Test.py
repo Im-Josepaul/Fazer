e1df1fb9f7df999f78f0a7b5f8a9fc7894b49629
Josepaul Jenson <64831240+Im-Josepaul@users.noreply.github.com>
2022-04-27 14:45:42 +0530

    Phazer Update 1
    Changing the path of the image

--- Face_Recognition_Test.py
+++ Face_Recognition_Test.py
@@ -14,7 +14,7 @@ import cv2
 video_capture = cv2.VideoCapture(0)
 
 # Load a sample picture and learn how to recognize it.
-obama_image = face_recognition.load_image_file("../Images/Josepaul_01.jpg")
+obama_image = face_recognition.load_image_file("/home/josepaul/Desktop/Area 51/Images/Josepaul_02.jpg")
 obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
 
 # Initialize some variables
